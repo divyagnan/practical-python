976d9c4f896d3be977e8c6e714493b6fb7f6583a
divyagnan <divyagnan@users.noreply.github.com>
2020-06-26 15:16:49 -0400

    1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+current_height = 100
+rebound_value = 3/5
+num_bounces = 10
+
+for i in range(num_bounces):
+    current_height *= rebound_value
+    # add 1 to the day since we are zero indexed
+    print(i + 1, round(current_height, 4))
