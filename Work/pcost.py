c3173eaecdee909153421549f8ffafdefae296e7
divyagnan <divyagnan@users.noreply.github.com>
2020-07-13 23:28:07 -0400

    1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+file = open('Data/portfolio.csv', 'rt')
+# skip headers
+headers = next(file)
+total_cost = 0.0
+# iterate through the file
+for line in file:
+    values = line.split(',')
+    total_cost += (int(values[1]) * float(values[2]))
+file.close()
+print('Total cost ', total_cost)
